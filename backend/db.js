b58ab8f3d514ebfbc053331e6e975958a8ca430f
GRaghav007 <raghavgoel915@gmail.com>
2024-09-09 03:11:54 +0530

    Made model, Schemas and Routes in Mongoose

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');   
-const mongoURI = 'mongodb+srv://GoFood:Raghav@1234@cluster0.ajdbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://GoFood:Raghav@1234@cluster0.ajdbh.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
 
 const mongoDB = async () => {
 
-    await mongoose.connect(mongoURI,{useNewUrlParser: true}, () => {
-        console.log("Connected");
+    await mongoose.connect(mongoURI,{useNewUrlParser: true}, async (err, result) => {
+        if(err){
+            console.log("----", err);
+        }
+        else{
+            console.log("Connected");
+            const fetched_data = await mongoose.connection.db.collection("fooddata");
+            fetched_data.find({}).toArray(function(err, data){
+                if(err) console.log(err);
+                // console.log(data)
+                else console.log("data fetched");
+            })
+        }
     })
 }
 
--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,7 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.use(express.json());
+app.use('/api', require("./Routes/CreateUser"));
+
 app.listen(5000)
