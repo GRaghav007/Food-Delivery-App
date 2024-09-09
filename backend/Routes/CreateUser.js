8fadcfd79c74828a0179ef6dbe64bb1c452744e4
GRaghav007 <raghavgoel915@gmail.com>
2024-09-10 01:53:28 +0530

    Used JWT Token and Bcryptjs

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwtSecret = "eakfrhgiuawehohfrwqalfendlaslhsdf";
 router.post("/createuser",[
     body('email', 'Incorrect email').isEmail(),
     body('password', 'Incorrect password').isLength({min: 5}),
@@ -15,10 +17,13 @@ router.post("/createuser",[
         return res.status(400).json({errors: errors.array()})
     }
 
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt);
+
     try {
         await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPassword,
             email: req.body.email,
             location: req.body.location
         })
@@ -44,12 +49,21 @@ router.post("/login",[
         if(!userData){
             return res.status(400).json({errors: "Wrong Credentials"});
         }
-
-        if(req.body.password !== userData.password){
+        
+        const pwdCompare = await bcrypt.compare(req.body.password, userData.password)
+        if(!pwdCompare){
             return res.status(400).json({errors: "Wrong Credentials"});
         }
 
-        return res.json({success: true});
+        const data = {
+            user:{
+                id: userData.id
+            }
+        }
+
+        const authToken = jwt.sign(data, jwtSecret);
+
+        return res.json({success: true, authToken: authToken});
     } catch (error) {
         console.log(error);
         res.json({success: false})
